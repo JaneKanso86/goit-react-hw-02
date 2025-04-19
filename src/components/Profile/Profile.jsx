f1ee30d46c6928d381c213c26dc9f14f0bc6a832
Jane Vasylieva <janekanso86@gmail.com>
2025-04-19 11:47:33 +0300

    add change

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,22 +4,23 @@ export default function Profile({ name, tag, location, image, stats }) {
   return (
     <div className={css.Profile}>
       <div className={css.description}>
-        <img src={image} alt="User avatar" className={css.avatar} /> {' '}
-        <p>{name}</p> <p>@{tag}</p>
-        <p>{location}</p> 
+        <img src={image} alt="User avatar" className={css.avatar} /> 
+        <p className={css.name}>{name}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p> 
       </div>
-      <ul>
+      <ul className={css.stats}>
         <li>
-          <span>Followers</span>
-          <span>{stats.followers}</span>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{stats.followers}</span>
         </li>
         <li>
-          <span>Views</span>
-          <span>{stats.views}</span>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{stats.views}</span>
         </li>
         <li>
-          <span>Likes</span>
-          <span>{stats.likes}</span>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
